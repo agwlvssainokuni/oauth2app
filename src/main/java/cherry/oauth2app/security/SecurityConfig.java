90ecfc1eae32bfc66c2e8fb913a34aed61258363
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2021-06-23 06:38:08 +0900

    OAuth2サンプル。ログアウト追加。

--- src/main/java/cherry/oauth2app/security/SecurityConfig.java
+++ src/main/java/cherry/oauth2app/security/SecurityConfig.java
@@ -16,12 +16,19 @@
 
 package cherry.oauth2app.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and() //
                 .logout() //
                 .logoutUrl("/logout") //
-                .logoutSuccessUrl("/") //
+                .logoutSuccessHandler(oidcLogoutSuccessHandler()) //
                 .invalidateHttpSession(true) //
                 .clearAuthentication(true) //
                 .and() //
@@ -38,6 +45,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/userinfo").authenticated() //
                 .antMatchers("/exit").authenticated() //
                 .antMatchers("/**").permitAll();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler handler = new OidcClientInitiatedLogoutSuccessHandler(
+                clientRegistrationRepository);
+        handler.setPostLogoutRedirectUri("{baseUrl}");
+        return handler;
     }
 
 }
